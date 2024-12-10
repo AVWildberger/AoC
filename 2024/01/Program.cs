22f17489c85083c0e6d1805fb393ed223c0e07c8
Andre <AVWildberger@gmail.com>
2024-12-10 19:09:44 +0100

    Add Logic for Star 1

--- 2024/01/Program.cs
+++ 2024/01/Program.cs
@@ -1,1 +1,34 @@
-﻿var input = File.ReadAllLines("../../../input.txt");
+﻿// Check/Read file inputs //
+if (!File.Exists("../../../input.txt"))
+{
+    Console.Write("Please ensure that the correct input file named \"input.txt\" is in the same directory as Program.cs!");
+    Console.ReadKey(true);
+    return;
+}
+
+var input = File.ReadAllLines("../../../input.txt");
+
+// Main logic //
+int[] left = new int[input.Length];
+int[] right = new int[input.Length];
+
+for (var i = 0; i < input.Length; i++)
+{
+    var split = input[i].Split("   ");
+    left[i] = int.Parse(split[0]);
+    right[i] = int.Parse(split[1]);
+}
+
+var sortedLeft = left.OrderBy(x => x);
+var sortedRight = right.OrderBy(x => x);
+
+var sum = 0;
+
+for (var i = 0; i < input.Length; i++)
+{
+    sum += Math.Abs(sortedRight.ElementAt(i) - sortedLeft.ElementAt(i));
+}
+
+// Print result //
+
+Console.WriteLine(sum);
