acd3e288262d5cec9588d617f90cead73db82c1d
Andre <AVWildberger@gmail.com>
2024-12-10 20:33:20 +0100

    Add Star 2

--- 2024/01/Program.cs
+++ 2024/01/Program.cs
@@ -1,5 +1,9 @@
 ﻿using TextCopy;
 
+// CONFIG //
+
+const Copy COPY = Copy.NONE;
+
 // CHECK & READ FILE INPUTS //
 
 if (!File.Exists("../../../input.txt"))
@@ -13,27 +17,79 @@ var input = File.ReadAllLines("../../../input.txt");
 
 // MAIN LOGIC //
 
-int[] left = new int[input.Length];
-int[] right = new int[input.Length];
+int star1 = Star1(input);
+int star2 = Star2(input);
 
-for (var i = 0; i < input.Length; i++)
+// PRINT (& COPY) RESULTS //
+
+Console.WriteLine($"Star 1: {star1}");
+Console.WriteLine($"Star 2: {star2}");
+
+switch (COPY)
 {
-    var split = input[i].Split("   ");
-    left[i] = int.Parse(split[0]);
-    right[i] = int.Parse(split[1]);
+    case Copy.STAR_ONE:
+        await ClipboardService.SetTextAsync(star1.ToString());
+        break;
+    case Copy.STAR_TWO:
+        await ClipboardService.SetTextAsync(star2.ToString());
+        break;
+};
+
+
+
+static int Star1(string[] input)
+{
+    var sum = 0;
+    SplitAndConvertInput(input, out int [] left, out int[] right);
+
+    var sortedLeft = left.OrderBy(x => x);
+    var sortedRight = right.OrderBy(x => x);
+
+    for (var i = 0; i < input.Length; i++)
+    {
+        sum += Math.Abs(sortedRight.ElementAt(i) - sortedLeft.ElementAt(i));
+    }
+
+    return sum;
 }
 
-var sortedLeft = left.OrderBy(x => x);
-var sortedRight = right.OrderBy(x => x);
-
-var sum = 0;
-
-for (var i = 0; i < input.Length; i++)
+static int Star2(string[] input)
 {
-    sum += Math.Abs(sortedRight.ElementAt(i) - sortedLeft.ElementAt(i));
+    var similarityScore = 0;
+
+    SplitAndConvertInput(input, out int[] left, out int[] right);
+
+    foreach (int item in left)
+    {
+        int count = 0;
+
+        foreach (var r in right)
+        {
+            if (r == item) count++;
+        }
+
+        similarityScore += count * item;
+    }
+
+    return similarityScore;
 }
 
-// PRINT & COPY RESULTS //
+static void SplitAndConvertInput(string[] input, out int[] left, out int[] right)
+{
+    left = new int[input.Length];
+    right = new int[input.Length];
 
-Console.WriteLine(sum);
-await ClipboardService.SetTextAsync(sum.ToString());
+    for (var i = 0; i < input.Length; i++)
+    {
+        var split = input[i].Split("   ");
+        left[i] = int.Parse(split[0]);
+        right[i] = int.Parse(split[1]);
+    }
+}
+
+enum Copy
+{
+    NONE,
+    STAR_ONE,
+    STAR_TWO
+}
