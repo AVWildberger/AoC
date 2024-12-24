cc033d9038eb7c89ecad74c0d0c35bc0d39f859f
Andre <AVWildberger@gmail.com>
2024-12-24 16:55:03 +0100

    Add Star 2 to 9/2024

--- 2024/09/Program.cs
+++ 2024/09/Program.cs
@@ -2,7 +2,7 @@
 
 // CONFIG //
 
-const Copy COPY = Copy.STAR_ONE;
+const Copy COPY = Copy.STAR_TWO;
 const string FILE = "../../../input.txt";
 
 // CHECK & READ FILE INPUTS //
@@ -45,28 +45,7 @@ switch (COPY)
 
 static long Star1(string input)
 {
-    List<char> chain = new();
-
-    var currentIndex = 0;
-    for (int i = 0; i < input.Length; i++)
-    {
-        var count = input[i] - '0';
-
-        for (int j = 0; j < count; j++)
-        {
-            if (i % 2 == 0)
-            {
-                chain.Add((char) (currentIndex + '0'));
-            }
-            else
-            {
-                chain.Add('.');
-            }
-        }
-
-        if (i % 2 == 0) { currentIndex++; }
-    }
-    Console.WriteLine();
+    List<char> chain = CreateChain(input);
 
     for (int i = 0; i < chain.Count; i++)
     {
@@ -85,21 +64,99 @@ static long Star1(string input)
         }
     }
 
+    return CalculateChecksum(chain);
+}
+
+static long Star2(string input)
+{
+    List<char> chain = CreateChain(input);
+
+    if (input.Length % 2 == 0)
+    {
+        chain.RemoveRange(input.Length - 1 - input[^1], input[^1]);
+        input = input.Substring(0, input.Length - 1);
+    }
+
+    int skipLast = 0;
+
+    for (int i = input.Length - 1; i > 0; i-=2)
+    {
+        int countDigits = Convert.ToInt32(input[i] - '0');
+        int countDots = Convert.ToInt32(input[i-1] - '0');
+        var index = FindIndexOfSequence(chain.ToArray(), new string('.', countDigits).ToCharArray());
+        var digitToMove = chain[^(skipLast + 1)];
+
+        if (index != -1 && index < chain.Count - skipLast)
+        {
+            for (int j = 0; j < countDigits; j++)
+            {
+                chain[index + j] = digitToMove;
+                chain[^(skipLast + j + 1)] = '.';
+            }
+        }
+
+        skipLast += countDigits;
+        skipLast += countDots;
+    }
+
+    return CalculateChecksum(chain);
+}
+
+// UTIL //
+
+static int FindIndexOfSequence(char[] arr, char[] sequence)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr.Skip(i).Take(sequence.Length).SequenceEqual(sequence))
+        {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+static long CalculateChecksum(List<char> chain)
+{
     long sum = 0;
     for (int i = 0; i < chain.Count; i++)
     {
-        sum += Convert.ToInt32(chain[i] - '0') * i;
+        if (chain[i] != '.')
+        {
+            sum += Convert.ToInt32(chain[i] - '0') * i;
+        }
     }
 
     return sum;
 }
 
-static int Star2(string input)
+static List<char> CreateChain(string input)
 {
-    return 0;
-}
+    List<char> chain = new();
 
-// UTIL //
+    var currentIndex = 0;
+    for (int i = 0; i < input.Length; i++)
+    {
+        var count = input[i] - '0';
+
+        for (int j = 0; j < count; j++)
+        {
+            if (i % 2 == 0)
+            {
+                chain.Add((char)(currentIndex + '0'));
+            }
+            else
+            {
+                chain.Add('.');
+            }
+        }
+
+        if (i % 2 == 0) { currentIndex++; }
+    }
+
+    return chain;
+}
 
 enum Copy
 {
